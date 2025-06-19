5dbd279f0c6771df17dc8ee3bb949be0a8d1e55f
yajathome <119949745+yajathome@users.noreply.github.com>
2025-06-19 14:00:54 +0530

    6tu4

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -28,15 +28,15 @@ function Team() {
                 <div className="" style={{"color": "#fff",paddingBottom: "40px"}}>
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous"></link>
                     <h1 style={{ textAlign:'center',fontSize: '50px',background: 'linear-gradient(90deg, #6366f1,rgb(153, 77, 235)',WebkitBackgroundClip: 'text',WebkitTextFillColor: 'transparent',}}>Team Management</h1>
-                    <div style={{textAlign:'center',fontSize:'30px'}}>    
-                        <p>Team Name | {team.team_name}</p>
-                        <br></br>
-                        <p>Team Count | {team.team_count}</p>
-                        <br />
-                        <p>Member Count | {team.member_names}</p>
-                        <br />
-                        <p>School Name | {team.school_name}</p>
-                        <br></br>
+                    <div style={{textAlign:'center',fontSize:'30px',paddingTop:'20px'}}>    
+                        <p style={{paddingBottom:'10px'}}>Team Name | {team.team_name}</p>
+                        
+                        <p style={{paddingBottom:'10px'}}>Team Count | {team.team_count}</p>
+                        
+                        <p style={{paddingBottom:'10px'}}>Member Count | {team.member_names}</p>
+               
+                        <p style={{paddingBottom:'10px'}}>School Name | {team.school_name}</p>
+                        
                         <a href="/project" style={{}}>See your projects</a>
                     </div>
                 </div>

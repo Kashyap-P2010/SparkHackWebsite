3570f4f5eed67d7446b6d7ede200eae9b5ab5ec3
yajathome <119949745+yajathome@users.noreply.github.com>
2025-06-19 13:28:52 +0530

    i57

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -28,7 +28,7 @@ function Team() {
                 <div className="" style={{"color": "#fff",paddingBottom: "40px"}}>
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous"></link>
                     <h1 style={{ textAlign:'center',fontSize: '50px',background: 'linear-gradient(90deg, #6366f1,rgb(153, 77, 235)',WebkitBackgroundClip: 'text',WebkitTextFillColor: 'transparent',}}>Team Management</h1>
-                    <div style={{fontSize:'15px',textAlign:'center'}}>    
+                    <div style={{textAlign:'center'}}>    
                         <h1 >Team Name | {team.team_name}</h1>
                         <h1>Team Count</h1>
                         <h1>{team.team_count}</h1>

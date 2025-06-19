82b300a65b14a411570f21312a123161720d1b0d
yajathome <119949745+yajathome@users.noreply.github.com>
2025-06-19 13:55:48 +0530

    yu

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -29,13 +29,14 @@ function Team() {
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous"></link>
                     <h1 style={{ textAlign:'center',fontSize: '50px',background: 'linear-gradient(90deg, #6366f1,rgb(153, 77, 235)',WebkitBackgroundClip: 'text',WebkitTextFillColor: 'transparent',}}>Team Management</h1>
                     <div style={{textAlign:'center',fontSize:'30px'}}>    
-                        <p >Team Name | {team.team_name}</p>
-                        <p>Team Count</p>
-                        <p>{team.team_count}</p>
-                        <p>Member Count</p>
-                        <p>{team.member_names}</p>
-                        <p>School Name</p>
-                        <p>{team.school_name}</p>
+                        <p>Team Name | {team.team_name}</p>
+                        <br></br>
+                        <p>Team Count | {team.team_count}</p>
+                        <br />
+                        <p>Member Count | {team.member_names}</p>
+                        <br />
+                        <p>School Name | {team.school_name}</p>
+                        <br></br>
                         <a href="/project" style={{}}>See your projects</a>
                     </div>
                 </div>

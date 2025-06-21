011358f7628727f8827270cbcea7d3c68a07d3ae
yajathome <119949745+yajathome@users.noreply.github.com>
2025-06-21 15:52:33 +0530

    er

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -42,23 +42,25 @@
     }, [])
 
         return (
-            <div style={{marginTop:'6%',paddingTop: '1%', paddingBottom: '2%',textAlign:'center',width:'50%',margin:'auto',fontFamily:'monospace',boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} className="container">
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous"></link>
-                {project &&
-                    <div style={{color:'white'}}>
-                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous"></link>
-                        <h1>Team Name {project.team_name}</h1>
-                        <h1>Project Repository </h1>
-                        <a href={project.project_repo}>{project.project_repo}</a>
-                        <h1>Image Link </h1>
-                        <a href={project.image_link}>{project.image_link} </a>
-                    </div>
-                }
+            <div style={{marginTop:'6%'}}>
+                <div style={{paddingTop: '1%', paddingBottom: '2%',textAlign:'center',width:'50%',margin:'auto',fontFamily:'monospace',boxShadow:" 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} className="container">
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous"></link>
+                    {project &&
+                        <div style={{color:'white'}}>
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous"></link>
+                            <h1>Team Name {project.team_name}</h1>
+                            <h1>Project Repository </h1>
+                            <a href={project.project_repo}>{project.project_repo}</a>
+                            <h1>Image Link </h1>
+                            <a href={project.image_link}>{project.image_link} </a>
+                        </div>
+                    }
 
-                <br />
-                <br />
+                    <br />
+                    <br />
 
-                <a href="/project-submit" className="btn" style={{textAlign:'center',background:'linear-gradient(90deg, #6366f1, #8b5cf6,rgb(205, 101, 189))',color:'white',marginBottom:'30px',borderRadius:'30px',padding:'10px'}}>Submit or change a project</a>
+                    <a href="/project-submit" className="btn" style={{textAlign:'center',background:'linear-gradient(90deg, #6366f1, #8b5cf6,rgb(205, 101, 189))',color:'white',marginBottom:'30px',borderRadius:'30px',padding:'10px'}}>Submit or change a project</a>
+                </div>
             </div>
         )
     }
